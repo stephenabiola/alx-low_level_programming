41b6712bb23b94972a18a38fe9f185a1d74a3ed6
stephen abiola <stephenabiola107@gmail.com>
2023-09-15 09:56:19 -0700

    task8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,9 @@ void print_diagonal(int n)
 {
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		int i, j;
@@ -16,9 +18,13 @@ void print_diagonal(int n)
 		for (i = 0 ; i < n ; i++)
 		{
 			for (j = 0 ; j <= i ; j++)
+			{
+				if (j == i)
+				 _putchar('\\');
+			else if (j > i)
 				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
+			}
+	_putchar('\n');	
 		}
 	}
 
