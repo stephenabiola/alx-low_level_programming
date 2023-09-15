78217df0033ef610761b1d5ea55a27349d07b347
stephen abiola <stephenabiola107@gmail.com>
2023-09-15 08:30:34 -0700

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - prints a staright line on the terminal
+ * @n: number of times the character('_')will be printed
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1 ; i <= 1 ; i++)
+			_putchar('_');
+	}
+
+}
