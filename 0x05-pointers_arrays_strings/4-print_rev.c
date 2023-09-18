1ba5eb82cccbcae53b9fd8ab9bffabf3dd406a9c
stephen abiola <stephenabiola107@gmail.com>
2023-09-18 12:52:26 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - imprema en reversa
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+
+	int 0;
+
+	while (*str != '/0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0 ; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+
+}
